a73156ec86f181f31960e384a6d9d1da92002597
h8m0ss <danarmenta32@gmail.com>
2024-09-06 22:59:46 -0600

    FEAT: Added basic API

--- api/main.go
+++ api/main.go
@@ -20,7 +20,7 @@ func CreateApi(router *gin.Engine) {
 
     // CONFIG ----------------------------------
     api.GET("/config", func(c *gin.Context) {
-      bytes, err := os.ReadFile("./config.json")
+      bytes, err := os.ReadFile("./frontend-config.json")
       if err != nil {
         fmt.Printf("err: %v\n", err)
         c.JSON(http.StatusOK, gin.H {
@@ -31,11 +31,28 @@ func CreateApi(router *gin.Engine) {
       c.Data(http.StatusOK, gin.MIMEJSON, bytes)
     })
     api.POST("/config", func(c *gin.Context) {
-  
+      // TODO: implement
     })
 
     // MODULES ----------------------------------
+    api.POST("/module/:module", func(c *gin.Context) {
+      slug := c.Param("module")
+      body, err := c.GetRawData()
+      if err != nil {
+        fmt.Printf("err: %v\n", err)
+        c.JSON(http.StatusBadRequest, gin.H {
+          "error": err.Error(),
+        })
+        return
+      }
 
+      fmt.Printf("slug: %v, body: %v\n", slug, string(body))
+      c.JSON(http.StatusOK, gin.H {
+        "message": "ok",
+        "slug": slug,
+        "body": string(body)
+      })
+    })
   }
 
 
